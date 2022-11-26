6e24908cb7118fbf76832cefbb1ae30e0732ba2d
Nevena Tufegdzic <nevenatufegdzic9@gmail.com>
2022-11-26 22:12:20 +0100

    NavView update
    User picture, full name, email se prikazuju, dodato Logout dugme.

--- app/src/main/java/com/example/nsiprojekat/activites/MainActivity.kt
+++ app/src/main/java/com/example/nsiprojekat/activites/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.nsiprojekat.activites
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -13,17 +17,23 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.nsiprojekat.R
 import com.example.nsiprojekat.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        auth = Firebase.auth
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
@@ -41,6 +51,15 @@ class MainActivity : AppCompatActivity() {
         ), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        val tvName: TextView = binding.navView.getHeaderView(0).findViewById(R.id.fullname)
+        tvName.text = auth.currentUser!!.displayName
+        val tvEmail: TextView = binding.navView.getHeaderView(0).findViewById(R.id.email)
+        tvEmail.text = auth.currentUser!!.email
+        val profilePic: ImageView = binding.navView.getHeaderView(0).findViewById(R.id.profilePic)
+        Glide.with(this).load(auth.currentUser!!.photoUrl).into(profilePic)
+        val tvLogout: TextView = binding.tVlogout
+        tvLogout.setOnClickListener{Logout()}
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -52,5 +71,12 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+    private fun Logout() {
+        auth.signOut()
+        val i: Intent = Intent(this, LoginRegistrationActivity::class.java)
+        i.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(i)
+        finish()
     }
 }
